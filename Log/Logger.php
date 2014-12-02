a3124b85699d18c4d1a535f7ae631f1d848cb869
Florian Preusner <florian@preusner.com>
2014-12-02 05:10:33 +0100

    temporarily deactivated context cause serializing closures is not allowed

--- Log/Logger.php
+++ Log/Logger.php
@@ -38,7 +38,31 @@ class Logger implements LoggerInterface {
      */
     public function log($level, $message, array $context = array()) {
 
-        $message = new LogMessage($level, $message, $context);
+        // @todo: reactivate context cause of serializing closures is not possible
+        //[1] => EightPoints\Bundle\GuzzleBundle\Log\LogMessage Object(
+        //    [level:EightPoints\Bundle\GuzzleBundle\Log\LogMessage:private] => info
+        //    [message:EightPoints\Bundle\GuzzleBundle\Log\LogMessage:private] => vagrant-debian64 Guzzle/5.0.3 curl/7.26.0 PHP/5.5.10-1~dotdeb.1 - [2014-12-02T03:57:38+00:00] "GET /restfullink?p2i_key=8d97e7b12a93172f&p2i_url=http%3A%2F%2Fwww.stuttgarter-zeitung.de%2F&p2i_fullpage=1&p2i_wait=5&p2i_imageformat=png /" 200 181
+        //    [context:EightPoints\Bundle\GuzzleBundle\Log\LogMessage:private] => Array(
+        //        [request] => GuzzleHttp\Message\Request Object(
+        //        [url:GuzzleHttp\Message\Request:private] => GuzzleHttp\Url Object(
+        //            [scheme:GuzzleHttp\Url:private] => http
+        //            [host:GuzzleHttp\Url:private] => api.page2images.com
+        //            [port:GuzzleHttp\Url:private] =>
+        //            [username:GuzzleHttp\Url:private] =>
+        //            [password:GuzzleHttp\Url:private] =>
+        //            [path:GuzzleHttp\Url:private] => /restfullink
+        //            [fragment:GuzzleHttp\Url:private] =>
+        //            [query:GuzzleHttp\Url:private] => GuzzleHttp\Query Object(
+        //                [encoding:GuzzleHttp\Query:private] => rawurlencode
+        //                [aggregator:GuzzleHttp\Query:private] => Closure Object(
+        //                    [static] => Array(
+        //                        [numericIndices] => 1
+        //                    )
+        //                    [parameter] => Array(
+        //                        [$data] =>
+        //                    )
+
+        $message = new LogMessage($level, $message, array());
 
         $this->messages[] = $message;
     } // end: log

3beebe0f123c86b944d52a0d232ee35303b889bc
Florian Preusner <florian@preusner.com>
2015-06-03 22:36:28 +0200

    added HttpDataCollectorTest "Tests are missing #3"

--- Tests/DataCollector/HttpDataCollectorTest.php
+++ Tests/DataCollector/HttpDataCollectorTest.php
@@ -15,8 +15,81 @@ use       EightPoints\Bundle\GuzzleBundle\DataCollector\HttpDataCollector;
  */
 class HttpDataCollectorTest extends \PHPUnit_Framework_TestCase {
 
-    public function test() {
+    /**
+     * @var
+     */
+    protected $logger;
 
-        $this->markTestSkipped('implement me');
-    }
+    /**
+     * SetUp: before executing each test function
+     *
+     * @author  Florian Preusner
+     * @version 2.1
+     * @since   2015-06
+     */
+    public function setUp() {
+
+        $this->logger = $this->getMockBuilder('EightPoints\Bundle\GuzzleBundle\Log\Logger')
+                             ->getMock();
+    } // end: setUp()
+
+    /**
+     * Test Constructor
+     *
+     * @author  Florian Preusner
+     * @version 2.1
+     * @since   2015-06
+     *
+     * @covers  EightPoints\Bundle\GuzzleBundle\Log\LogGroup::construct
+     */
+    public function testConstruct() {
+
+        $collector = new HttpDataCollector($this->logger);
+        $data      = unserialize($collector->serialize());
+        $expected  = array(
+            'logs'      => array(),
+            'callCount' => 0,
+        );
+
+        $this->assertSame($expected, $data);
+    } // end: testConstruct()
+
+    /**
+     * Test Collecting Data
+     *
+     * @author  Florian Preusner
+     * @version 2.1
+     * @since   2015-06
+     *
+     * @covers  EightPoints\Bundle\GuzzleBundle\Log\LogGroup::collect
+     */
+    public function testCollect() {
+
+        $this->logger->expects($this->once())
+                     ->method('getMessages')
+                     ->willReturn(array('test message'));
+
+        $collector = new HttpDataCollector($this->logger);
+        $response  = $this->getMockBuilder('Symfony\Component\HttpFoundation\Response')
+                          ->getMock();
+        $request   = $this->getMockBuilder('Symfony\Component\HttpFoundation\Request')
+                          ->getMock();
+
+        $request->expects($this->once())
+                ->method('getPathInfo')
+                ->willReturn('id');
+
+        $collector->collect($request, $response);
+
+        $logs = $collector->getLogs();
+
+        /** @var \EightPoints\Bundle\GuzzleBundle\Log\LogGroup $log */
+        foreach($logs as $log) {
+
+            $this->assertInstanceOf('EightPoints\Bundle\GuzzleBundle\Log\LogGroup', $log);
+
+            $this->assertSame(array('test message'), $log->getMessages());
+            $this->assertSame('id', $log->getRequestName());
+        }
+    } // end: testCollect()
 } // end: HttpDataCollectorTest

312a0e30a097428923e2a70538b8028d4350f23a
Florian Preusner <florian@preusner.com>
2015-06-04 23:21:37 +0200

    extended HttpDataCollectorTest "Tests are missing #3"

--- Tests/DataCollector/HttpDataCollectorTest.php
+++ Tests/DataCollector/HttpDataCollectorTest.php
@@ -39,6 +39,8 @@ class HttpDataCollectorTest extends \PHPUnit_Framework_TestCase {
      * @author  Florian Preusner
      * @version 2.1
      * @since   2015-06
+     *
+     * @covers  EightPoints\Bundle\GuzzleBundle\DataCollector\HttpDataCollector::__construct
      */
     public function testConstruct() {
 
@@ -60,6 +62,8 @@ class HttpDataCollectorTest extends \PHPUnit_Framework_TestCase {
      * @since   2015-06
      *
      * @covers  EightPoints\Bundle\GuzzleBundle\DataCollector\HttpDataCollector::collect
+     * @covers  EightPoints\Bundle\GuzzleBundle\DataCollector\HttpDataCollector::getLogs
+     * @covers  EightPoints\Bundle\GuzzleBundle\DataCollector\HttpDataCollector::getLogGroup
      */
     public function testCollect() {
 
@@ -90,4 +94,115 @@ class HttpDataCollectorTest extends \PHPUnit_Framework_TestCase {
             $this->assertSame('id', $log->getRequestName());
         }
     } // end: testCollect()
+
+    /**
+     * Test Collector Name
+     *
+     * @author  Florian Preusner
+     * @version 2.1
+     * @since   2015-06
+     *
+     * @covers  EightPoints\Bundle\GuzzleBundle\DataCollector\HttpDataCollector::getName
+     */
+    public function testName() {
+
+        $collector = new HttpDataCollector($this->logger);
+
+        $this->assertSame('guzzle', $collector->getName());
+    } // end: testName()
+
+    /**
+     * Test Log Messages
+     *
+     * @author  Florian Preusner
+     * @version 2.1
+     * @since   2015-06
+     *
+     * @covers  EightPoints\Bundle\GuzzleBundle\DataCollector\HttpDataCollector::getMessages
+     */
+    public function testMessages() {
+
+        $this->logger->expects($this->once())
+                     ->method('getMessages')
+                     ->willReturn(['test message #1', 'test message #2']);
+
+        $collector = new HttpDataCollector($this->logger);
+        $response  = $this->getMockBuilder('Symfony\Component\HttpFoundation\Response')
+                          ->getMock();
+        $request   = $this->getMockBuilder('Symfony\Component\HttpFoundation\Request')
+                          ->getMock();
+
+        $request->expects($this->once())
+                ->method('getPathInfo')
+                ->willReturn('id');
+
+        $collector->collect($request, $response);
+
+        $messages = $collector->getMessages();
+
+        /** @var \EightPoints\Bundle\GuzzleBundle\Log\LogMessage $message */
+        foreach($messages as $i => $message) {
+
+            $text = sprintf('test message #%d', ($i + 1));
+            $this->assertSame($text, $message);
+        }
+    } // end: testMessages()
+
+    /**
+     * Test Call Count
+     *
+     * @author  Florian Preusner
+     * @version 2.1
+     * @since   2015-06
+     *
+     * @covers  EightPoints\Bundle\GuzzleBundle\DataCollector\HttpDataCollector::getCallCount
+     */
+    public function testCallCount() {
+
+        $this->logger->expects($this->once())
+                     ->method('getMessages')
+                     ->willReturn(['test message #1', 'test message #2']);
+
+        $collector = new HttpDataCollector($this->logger);
+        $response  = $this->getMockBuilder('Symfony\Component\HttpFoundation\Response')
+                          ->getMock();
+        $request   = $this->getMockBuilder('Symfony\Component\HttpFoundation\Request')
+                          ->getMock();
+
+        $request->expects($this->once())
+                ->method('getPathInfo')
+                ->willReturn('id');
+
+        $collector->collect($request, $response);
+
+        $this->assertSame(2, $collector->getCallCount());
+    } // end: testCallCount()
+
+    /**
+     * Test Error Count
+     *
+     * @author  Florian Preusner
+     * @version 2.1
+     * @since   2015-06
+     *
+     * @covers  EightPoints\Bundle\GuzzleBundle\DataCollector\HttpDataCollector::getErrorCount
+     */
+    public function testErrorCount() {
+
+        // implement me
+    } // end: testErrorCount()
+
+    /**
+     * Test Total Time
+     *
+     * @author  Florian Preusner
+     * @version 2.1
+     * @since   2015-06
+     *
+     * @covers  EightPoints\Bundle\GuzzleBundle\DataCollector\HttpDataCollector::getTotalTime
+     */
+    public function testTotalTime() {
+
+        // implement me
+    } // end: testTotalTime()
 } // end: HttpDataCollectorTest

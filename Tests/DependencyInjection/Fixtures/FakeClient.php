86650a5150184dc6906273d1d9bc39c6b93bbd06
Sebastian Blum <github@sblum.de>
2016-02-02 23:39:56 +0100

    Add missing Class FakeClient

---
+++ Tests/DependencyInjection/Fixtures/FakeClient.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EightPoints\Bundle\GuzzleBundle\Tests\DependencyInjection\Fixtures;
+
+
+/**
+ * Class FakeClient
+ *
+ * @package EightPoints\Bundle\GuzzleBundle\Tests\DependencyInjection\Fixtures
+ * @author Sebastian Blum
+ *
+ * @since 2016-01
+ */
+class FakeClient
+{
+
+}

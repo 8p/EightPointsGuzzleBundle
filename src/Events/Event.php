c2343584a3e902992363d588863c8b35cc70280d
Vlad Gregurco <gregurco.vlad@gmail.com>
2019-09-18 14:20:49 +0300

    Adjust code for deprecation messages appeared in SF 4.3.

--- src/Events/Event.php
+++ src/Events/Event.php
@@ -7,6 +7,13 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Contracts\EventDispatcher\Event as ContractsBaseEvent;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
+/**
+ * @see: https://github.com/8p/EightPointsGuzzleBundle/pull/261
+ * @see: https://github.com/8p/EightPointsGuzzleBundle/pull/265
+ *
+ * SF 4.3 introduced Contracts and deprecated interfaces used before.
+ * This file is a layer to support different versions and don't cause deprecation messages.
+ */
 if (is_subclass_of(EventDispatcher::class, EventDispatcherInterface::class)) {
     class Event extends ContractsBaseEvent
     {

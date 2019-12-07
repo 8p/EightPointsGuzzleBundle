56830f58aa0c540c4c45fe52ba9e5c22f5f3079f
Remon van de Kamp <rpkamp@gmail.com>
2019-12-07 21:03:49 +0100

    Add deprecations for GuzzleEventListenerInterface

--- src/Events/GuzzleEventListenerInterface.php
+++ src/Events/GuzzleEventListenerInterface.php
@@ -2,6 +2,13 @@
 
 namespace EightPoints\Bundle\GuzzleBundle\Events;
 
+use function trigger_error;
+
+@trigger_error(
+    sprintf('Interface "%s" is deprecated and will be removed in EightPointsGuzzleBundle version 8', GuzzleEventListenerInterface::class),
+    E_USER_DEPRECATED
+);
+
 interface GuzzleEventListenerInterface
 {
     /**

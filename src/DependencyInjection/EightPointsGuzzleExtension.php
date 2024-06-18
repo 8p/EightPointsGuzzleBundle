e15d3c5a99d0fc2b67e01d697000be1b12f02638
Vlad Gregurco <gregurco.vlad@gmail.com>
2024-06-18 14:21:30 +0300

    Fix Symfony Extension class deprecation

--- src/DependencyInjection/EightPointsGuzzleExtension.php
+++ src/DependencyInjection/EightPointsGuzzleExtension.php
@@ -6,11 +6,11 @@ use EightPoints\Bundle\GuzzleBundle\Log\Logger;
 use EightPoints\Bundle\GuzzleBundle\Twig\Extension\DebugExtension;
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\ExpressionLanguage\Expression;
 use GuzzleHttp\HandlerStack;
 
